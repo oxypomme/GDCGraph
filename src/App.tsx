b29c5530f69691861757eee01f62c9c044ecac5f
OxyTom <oxytom8@gmail.com>
2021-03-21 00:01:45 +0100

    created project

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+import Navbar from "./features/commons/Navbar";
+
+import Home from "./views/home";
+
+import NotFound from "./views/errors/NotFound";
+
+const AppContainer = styled.div`
+  margin-top: var(--nav-size);
+  text-align: center;
+  min-height: calc(100vh - var(--nav-size) - var(--footer-size) - 14px);
+`;
+
+const App = (): JSX.Element => {
+  return (
+    <Router basename='/'>
+      <Navbar />
+      <AppContainer>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/players/:id" component={Home} />
+          <Route exact path="/missions/:id" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </AppContainer>
+    </Router>
+  );
+}
+
+export default App;
