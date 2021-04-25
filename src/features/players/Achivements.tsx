a4244467a370882b9670994237c26da347630352
OxyTom <oxytom8@gmail.com>
2021-04-25 18:14:10 +0200

    added Leader achievement

--- src/features/players/Achivements.tsx
+++ src/features/players/Achivements.tsx
@@ -1,96 +1,142 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
-import IPlayerType from '@/models/IPlayerType';
-import ReactTooltip from 'react-tooltip';
+import IPlayerType from "@/models/IPlayerType";
+import ReactTooltip from "react-tooltip";
 
-import playablesDays from '@config/playablesDays.json';
-import dayjs from 'dayjs';
+import playablesDays from "@config/playablesDays.json";
+import dayjs from "dayjs";
 
 const Card = styled.div`
-    background: var(--background-dark);
-    width: 100%;
-    padding: 10px;
-    margin: 10px;
-    border-radius: 5px;
-    & > h4 {
-        margin: 0;
-    }
+  background: var(--background-dark);
+  width: 100%;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  & > h4 {
+    margin: 0;
+  }
 `;
 
 type PropsType = {
-    player: IPlayerType
-}
+  player: IPlayerType;
+};
 
 interface AchivementType {
-    name: string,
-    icon: string,
-    desc: string
+  name: string;
+  icon: string;
+  desc: string;
 }
+
+const UNLOCK_PERCENT = 40 / 100;
+const NB_MISSION_MIN = 400;
 
 const Achivements = (props: PropsType): JSX.Element => {
-    const { player } = props;
+  const { player } = props;
 
-    const [achivements, setAchivements] = React.useState<AchivementType[]>([]);
+  const [achivements, setAchivements] = React.useState<AchivementType[]>([]);
 
-    React.useEffect(() => {
-        const achs: AchivementType[] = [];
-        if (player) {
-            if (player.total_player_mission_status.SUCCES_Mort / player.total_mission_status.SUCCES >= .4) {
-                achs.push({
-                    name: "Elle a fini sans toi",
-                    icon: "",
-                    desc: `Est mort dans ${(player.total_player_mission_status.SUCCES_Mort / player.total_mission_status.SUCCES * 100).toLocaleString(undefined, { maximumFractionDigits: 2 })}% de ses missions accomplies`
-                });
-            }
+  React.useEffect(() => {
+    const achs: AchivementType[] = [];
+    if (player) {
+      if (
+        player.total_player_mission_status.SUCCES_Mort /
+          player.total_mission_status.SUCCES >=
+        UNLOCK_PERCENT
+      ) {
+        achs.push({
+          name: "Elle a fini sans toi",
+          icon: "",
+          desc: `Est mort dans ${(
+            (player.total_player_mission_status.SUCCES_Mort /
+              player.total_mission_status.SUCCES) *
+            100
+          ).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+          })}% de ses missions accomplies`,
+        });
+      }
 
-            if (player.count_missions >= 400) {
-                achs.push({
-                    name: "Petit joueur",
-                    icon: "",
-                    desc: `A joué seulement ${player.count_missions} missions`
-                });
-            }
+      if (player.count_missions >= NB_MISSION_MIN) {
+        achs.push({
+          name: "Petit joueur",
+          icon: "",
+          desc: `A joué seulement ${player.count_missions} missions`,
+        });
+      }
 
-            const sortedDays = Object.keys(player.days).map((key, i) => ({
-                day: dayjs().day(parseInt(key)).format('dddd'),
-                count: Object.values(player.days)[i].count
-            })).sort((a, b) => b.count - a.count);
-            if (sortedDays[0]?.count / player?.count_missions >= .4) {
-                achs.push({
-                    name: `Joueur du ${sortedDays[0].day}`,
-                    icon: "",
-                    desc: `A joué ${(sortedDays[0].count / player.count_missions * 100).toLocaleString(undefined, { maximumFractionDigits: 2 })}% de ses missions le ${sortedDays[0].day}`
-                });
-            }
+      const sortedDays = Object.keys(player.days)
+        .map((key, i) => ({
+          day: dayjs().day(parseInt(key)).format("dddd"),
+          count: Object.values(player.days)[i].count,
+        }))
+        .sort((a, b) => b.count - a.count);
+      if (sortedDays[0]?.count / player?.count_missions >= UNLOCK_PERCENT) {
+        achs.push({
+          name: `Joueur du ${sortedDays[0].day}`,
+          icon: "",
+          desc: `A joué ${(
+            (sortedDays[0].count / player.count_missions) *
+            100
+          ).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+          })}% de ses missions le ${sortedDays[0].day}`,
+        });
+      }
 
-            const sortedRoles = Object.keys(player.roles.roles_count).map((key, i) => ({
-                name: key,
-                count: Object.values(player?.roles.roles_count)[i]
-            })).sort((a, b) => b.count - a.count);
-            if (sortedRoles[0]?.count / player?.count_missions >= .4) {
-                achs.push({
-                    name: `Éternel ${sortedRoles[0].name}`,
-                    icon: "",
-                    desc: `A joué ${(sortedRoles[0].count / player.count_missions * 100).toLocaleString(undefined, { maximumFractionDigits: 2 })}% de ses missions en tant que ${sortedRoles[0].name}`
-                });
-            }
+      const sortedRoles = Object.keys(player.roles.roles_count)
+        .map((key, i) => ({
+          name: key,
+          count: Object.values(player?.roles.roles_count)[i],
+        }))
+        .sort((a, b) => b.count - a.count);
+      if (sortedRoles[0]?.count / player?.count_missions >= UNLOCK_PERCENT) {
+        achs.push({
+          name: `Éternel ${sortedRoles[0].name}`,
+          icon: "",
+          desc: `A joué ${(
+            (sortedRoles[0].count / player.count_missions) *
+            100
+          ).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+          })}% de ses missions en tant que ${sortedRoles[0].name}`,
+        });
+      }
+
+      let leaderCount = 0;
+      for (const role of sortedRoles) {
+        if (role.name.includes("Leader")) {
+          leaderCount += role.count;
         }
-        setAchivements(achs);
-    }, [player]);
+      }
+      if (leaderCount / player?.count_missions >= UNLOCK_PERCENT) {
+        achs.push({
+          name: "Leader un jour, Leader toujours",
+          icon: "",
+          desc: `A joué ${(
+            (leaderCount / player.count_missions) *
+            100
+          ).toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+          })}% de ses missions en tant que Leader (SL, TL ou Leader)`,
+        });
+      }
+    }
+    setAchivements(achs);
+  }, [player]);
 
-    return (
+  return (
+    <>
+      {achivements.map((a, id) => (
         <>
-            {achivements.map((a, id) => (
-                <>
-                    <Card key={id} data-tip={a.desc}>
-                        <h4>{a.name}</h4>
-                    </Card>
-                    <ReactTooltip />
-                </>
-            ))}
+          <Card key={id} data-tip={a.desc}>
+            <h4>{a.name}</h4>
+          </Card>
+          <ReactTooltip />
         </>
-    );
-}
+      ))}
+    </>
+  );
+};
 
 export default Achivements;
